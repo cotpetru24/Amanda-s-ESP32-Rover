4a193657762fdef921c258d59b0c75752c812f71
cotpetru24 <cotpetru1@gmail.com>
2026-08-04 16:23:27 +0100

    updated ultrasonic pins

--- include/UltrasonicController.h
+++ include/UltrasonicController.h
@@ -7,7 +7,7 @@ public:
     float getDistanceCentimetres();
 
 private:
-    static constexpr int TriggerPin = 34;
+    static constexpr int TriggerPin = 21;
     static constexpr int EchoPin = 35;
 
     static constexpr unsigned long EchoTimeoutMicroseconds = 30000;
