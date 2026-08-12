f0c4c83d61b988b3a3ee2fecf0040e2c6d6db74a
petruc <petruc@CTTC-LT-32596>
2026-08-12 17:02:13 +0100

    adding the DisplayController

---
+++ include/DisplayController.h
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+class DisplayController
+{
+public:
+    void begin();
+
+    void showReady();
+    void showMessage(const char *title, const char *message);
+    void showStatus(const char *mode, const char *movement, float distance);
+
+private:
+    static constexpr int ScreenWidth = 128;
+    static constexpr int ScreenHeight = 64;
+
+    static constexpr int SdaPin = 18;
+    static constexpr int SclPin = 19;
+
+    static constexpr int ResetPin = -1;
+    static constexpr uint8_t DisplayAddress = 0x3C;
+
+    Adafruit_SSD1306 display { ScreenWidth, ScreenHeight, &Wire, ResetPin };
+
+    bool initialized = false;
+};
